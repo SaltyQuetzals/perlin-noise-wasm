abe9567358bd92b2017da606a407f83bc22abfb7
SaltyQuetzals <quontas@gmail.com>
2020-08-02 23:33:52 +0000

    Deploying to gh-pages from  @ 2a7a4157432c907a30188dba8ebc56adf9fb9171 🚀

---
+++ index.js
@@ -0,0 +1,21 @@
+import * as wasm from "wasm-perlin-noise";
+
+const drawCells = () => {
+  const WIDTH = window.innerWidth;
+  const HEIGHT = window.innerHeight;
+
+  let z = Math.floor(Math.random() * 256);
+  /** @type {HTMLCanvasElement} */
+  const canvas = document.getElementById("perlin-noise-canvas");
+  canvas.width = WIDTH;
+  canvas.height = HEIGHT;
+  canvas.style.width = `${WIDTH}px`;
+  canvas.style.height = `${HEIGHT}px`;
+  const ctx = canvas.getContext('2d');
+  wasm.draw(ctx, canvas.width, canvas.height, z);
+};
+
+drawCells();
+
+
+window.onresize = drawCells;
